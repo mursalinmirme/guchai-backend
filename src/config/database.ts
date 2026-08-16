dd08ca7b702624f2a7c3335c9712775be9d2b6b7
Mursalin Mir <mursalinmir02@gmail.com>
2026-08-16 23:01:45 +0600

    fix: db issue

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
+    if (mongoose.connection.readyState >= 1) return;
     const conn = await mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/guchai");
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
